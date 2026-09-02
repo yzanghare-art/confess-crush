b1d50dc75117f081294f4055f1f4611b8c87e485
yzanghare-art <yzanghare@gmail.com>
2026-09-03 03:31:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const CONFIG = {
     btnNo: 'hg nha :3',
     question: ' Em Thích Anh Ở Điểm Nào Dợ',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Tại anh béo',
+    reply: 'Tại anh béo úuuuu',
     mess: 'Biết ngay mà. Yêu quãi🦭🦭🦭',
     messDesc: 'Te amo.',
     btnAccept: 'Okiiiii',
