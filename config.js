a90eb2c80cfd36bc8fe40372a90a6533ba0a6207
yzanghare-art <yzanghare@gmail.com>
2026-09-03 03:34:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,10 +9,10 @@ const CONFIG = {
     btnYes: 'dạ coá',
     btnNo: 'hg nha :3',
     question: ' Em Thích Anh Ở Điểm Nào Dợ',
-    btnReply: 'Gửi cho bạn <3',
+    btnReply: 'Gửi ảnh🦭',
     reply: 'Tại anh béo úuuuu',
-    mess: 'Biết ngay mà. Yêu quãi🦭🦭🦭',
+    mess: 'Biết ngay mà 😆.💗Yêu quãi💗',
     messDesc: 'Te amo.',
-    btnAccept: 'Okiiiii',
+    btnAccept: 'Ogeee',
     messLink: 'https://music.youtube.com/watch?v=EiS7cKfuf6w' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
